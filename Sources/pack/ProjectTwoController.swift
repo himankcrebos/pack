50308cc74e94c627df3d2b7082229bfde51400df
himankcrebos <himank.crebos@gmail.com>
2024-04-11 14:02:04 +0530

    test

---
+++ Sources/pack/ProjectTwoController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ProjectTwo
+//
+//  Created by Himank Bansal on 11/04/24.
+//
+
+import UIKit
+
+public class ProjectTwoController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        prinData()
+    }
+    
+    public func prinData(){
+        print("Hello")
+    }
+
+
+}
+
